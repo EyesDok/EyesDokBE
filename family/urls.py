4bd448440b2820f82359bbc821fdfc7aa3f78f61
Beomtae <95863810+Beomtae@users.noreply.github.com>
2023-08-14 01:00:01 +0900

    Family 모델 추가

--- family/urls.py
+++ family/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import myFamily_view
+from .views import myFamily_view ,create_family_view
 # from .views import 
 
 app_name = 'family'
 
 urlpatterns = [
     path('', myFamily_view, name='myFamily'),
-    # path('login/', login_view, name='login'),
-    # path('logout/', logout_view, name='logout'),
-    # path('check-username/', check_username_view, name='check-username'),
+    path('create', create_family_view, name='createFamily')
 ]
