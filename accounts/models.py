0d8ea7cf79c32ed635e5cab15afc48eb71b97214
Beomtae <95863810+Beomtae@users.noreply.github.com>
2023-08-14 00:08:26 +0900

    db 수정

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class FamilyGroup(models.Model):
-    name = models.CharField(max_length=150)
-    members = models.ManyToManyField('CustomUser', related_name='families')
-
-    def __str__(self):
-        return self.name
-
 class CustomUser(AbstractUser):
     membership = models.CharField(max_length=150, default=False)
-    family = models.ForeignKey(FamilyGroup, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.username
