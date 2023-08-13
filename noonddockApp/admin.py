adf3609c32d07bd1aad6745594aa8a51b51ebf78
drgnseok <108001842+drgnseok@users.noreply.github.com>
2023-08-13 18:30:50 +0900

    Add NoonDDock admin
    admin 추가

--- noonddockApp/admin.py
+++ noonddockApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Photo
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'recommend')
+
+admin.site.register(Post, PostAdmin)
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('image', 'post_id')
+
+admin.site.register(Photo, PhotoAdmin)
--- noonddockApp/models.py
+++ noonddockApp/models.py
@@ -3,9 +3,12 @@ from django.db import models
 class Post(models.Model):
     content     =   models.TextField("CONTENT", max_length=300, default="포스트 내용") #포스트 내용
     title       =   models.CharField("TITLE", max_length=20, default="포스트 제목") #포스트 제목
-    pub_date    =   models.DateField("DATE", auto_now_add=True) #작성일자
+    pub_date    =   models.DateField("PUB_DATE", auto_now_add=True) #작성일자
     recommend   =   models.PositiveIntegerField("RECOMMEND", default=0) #추천수수집을 위한 모델
+
+    def __str__(self):
+        return self.title
 
 class Photo(models.Model):
     post_id     =   models.ForeignKey(Post, on_delete=models.CASCADE, default=1) #PostFK
-    image       =   models.ImageField("IMAGE", upload_to="/", default="static/img/") #포스트 사진
+    image       =   models.ImageField("IMAGE", upload_to="postImage/", default="static/img/defaultImg.png") #포스트 사진
