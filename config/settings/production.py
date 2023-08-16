ef148763225d5f3016fe8d08b1d410f88d4f951d
Beomtae <95863810+Beomtae@users.noreply.github.com>
2023-08-16 16:57:23 +0900

    배포 준비 3

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['naver.com'] # 도메인, IP
+ALLOWED_HOSTS = ['15.164.228.179'] # 도메인, IP
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
