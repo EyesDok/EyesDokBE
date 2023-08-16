97939bf244ca82388aa511cb2e0c9c71adfc4ef7
Beomtae <95863810+Beomtae@users.noreply.github.com>
2023-08-16 20:11:41 +0900

    도메인 주소 설정 3

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,10 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ # 도메인, IP
-    '15.164.228.179',
-    'noonddock.site',
-    ] 
+ALLOWED_HOSTS = ['noonddock.site'] 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
