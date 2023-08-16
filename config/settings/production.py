06fb8ce1bb4f6998f04131052f0c99b785032397
Beomtae <95863810+Beomtae@users.noreply.github.com>
2023-08-16 20:53:42 +0900

    다시 마음잡고 해보자

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['noonddock.site'] 
+ALLOWED_HOSTS = ['15.164.228.179'] 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
